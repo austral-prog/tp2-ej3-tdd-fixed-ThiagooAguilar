3267e2c4d1cbdf1a8b944607d08aaae47e7419c8
thiagoaguilar <taguilar@mail.austral.edu.ar>
2024-08-23 12:55:30 -0300

    proyecto terminado

--- src/main/java/com/template/App.java
+++ src/main/java/com/template/App.java
@@ -1,14 +1,79 @@
 package com.template;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class App {
 
 
     public static Map<String, Integer> deposit(Map<String, Integer> accounts, String account, int amount) {
-        return Collections.emptyMap();
+        if (amount < 0) {
+            System.out.println("Realizar un deposito negativo no se permite.");
+            return accounts;
+        }else if (!accounts.containsKey(account)) {
+            System.out.println("Realizar un deposito en una cuenta no existente no se permite.");
+            return accounts;
+        }
+        else {
+            int currentMoney = accounts.get(account);
+            int newMoney = currentMoney + amount;
+            accounts.put(account, newMoney);
+            return accounts;
+        }
+
     }
 
+    public static Map<String, Integer> CrearCuentaConSaldoInicial(String nombre) {
+        Map<String, Integer> cuenta = new HashMap();
+        int Money = 0;
+        cuenta.put(nombre, Money);
+        return cuenta;
+    }
+    public static Map<String, Integer> retire(Map<String, Integer> accounts, String account, int amount) {
+        if (amount < 0) {
+            System.out.print("Realizar un retiro negativo no se permite.");
+            return accounts;
+        } else if (!accounts.containsKey(account)) {
+            System.out.print("Realizar un retiro en una cuenta no existente no se permite");
+            return accounts;
+        }else if (accounts.get(account) == 0){
+            System.out.print("Realizar un retiro en la cuenta sin saldo no se permite.");
+            return accounts;
+        }
+        else {
+            int currentMoney = accounts.get(account);
+            int newMoney = currentMoney - amount;
+            accounts.put(account, newMoney);
+            return accounts;
+        }
+    }
+    public static Map<String, Integer> Transference(Map<String, Integer> accounts, String personaquetransfiere, String personaquerecibe, int monto) {
 
+        if (!accounts.containsKey(personaquetransfiere)) {
+            System.out.print("Realizar una transferencia entre cuentas no existentes no se permite.");
+            return accounts;
+        }
+        else if (accounts.get(personaquetransfiere)==0) {
+            System.out.print("Realizar una transferencia entre cuentas sin saldo no se permite.");
+            return accounts;
+        } else if (monto<0){
+            System.out.print("Realizar una transferencia negativa no se permite.");
+            return accounts;
+        }
+        else if (personaquetransfiere.equals(personaquerecibe)){
+            System.out.print("Realizar una transferiencia entre la misma cuenta no se permite");
+            return accounts;
+        }
+        else {
+            int currentMoney = accounts.get(personaquetransfiere);
+            int newMoney = currentMoney - monto;
+            int currentMoney2 = accounts.get(personaquerecibe);
+            int newMoney2 = currentMoney2 + monto;
+            accounts.put(personaquetransfiere, newMoney);
+            accounts.put(personaquerecibe, newMoney2);
+            return accounts;
+        }
+    }
 }
+
--- src/test/java/com/template/AppTest.java
+++ src/test/java/com/template/AppTest.java
@@ -2,17 +2,130 @@ package com.template;
 
 import org.junit.jupiter.api.Test;
 
+import javax.xml.validation.Validator;
+import java.util.HashMap;
 import java.util.Map;
 
-import static com.template.App.deposit;
+import static com.template.App.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AppTest {
 
     @Test
     void testDeposit() {
-        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 15);
         Map<String, Integer> updatedAccounts = deposit(accounts, "mati", 5);
-        assertEquals(true, true);
+        assertEquals(15, updatedAccounts.get("mati"));
+    }
+    @Test
+    void testCrearCuentaConSaldoInicial() {
+        Map<String, Integer> cuenta = new HashMap<>();
+        Map<String, Integer> cuenta_creada = CrearCuentaConSaldoInicial("Thiago");
+        assertEquals(0,cuenta_creada.get("Thiago"));
+    }
+    @Test
+    void testSaldonegativo() {
+        Map<String , Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 15);
+        Map<String,Integer> confirmacion = deposit( accounts,"mati", -20);
+        assertEquals(15, confirmacion.get("juan"));
+        assertEquals(10, confirmacion.get("mati"));
+
+    }
+    @Test
+    void testNoExistencia() {
+        Map<String , Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 15);
+        Map<String,Integer> existence = deposit( accounts,"Francisco", 20);
+        assertEquals(15, existence.get("juan"));
+        assertEquals(10, existence.get("mati"));
+    }
+    @Test
+    void testRetire() {
+        Map<String , Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 15);
+        Map<String, Integer> updateaccounts = retire(accounts, "mati", 9);
+        assertEquals(1, updateaccounts.get("mati"));
+    }
+    @Test
+    void testnegativeretire() {
+        Map<String , Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 15);
+        Map<String,Integer> updateaccounts = retire(accounts, "mati", -9);
+        assertEquals(10,updateaccounts.get("mati"));
+        assertEquals(15,updateaccounts.get("juan"));
+    }
+    @Test
+    void testretireexistence() {
+        Map<String , Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 15);
+        Map<String,Integer> updateaccounts = retire(accounts, "Federico", 20 );
+        assertEquals(15, updateaccounts.get("juan"));
+        assertEquals(10,updateaccounts.get("mati"));
+    }
+    @Test
+    void testretirezero() {
+        Map<String , Integer> accounts = new HashMap<>();
+        accounts.put("mati", 0);
+        accounts.put("juan", 15);
+        Map<String,Integer> mensaje = retire(accounts, "mati", 30);
+        assertEquals(15, mensaje.get("juan"));
+        assertEquals(0, mensaje.get("mati"));
+    }
+    @Test
+    void testTransference() {
+        Map<String , Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 15);
+        Map<String, Integer> updateaccounts = Transference(accounts, "juan","mati", 5 );
+        assertEquals(10,updateaccounts.get("juan"));
+        assertEquals(15,updateaccounts.get("mati"));
+    }
+
+    @Test
+    void testTransferencezero() {
+        Map<String , Integer> accounts = new HashMap<>();
+        accounts.put("mati", 0);
+        accounts.put("juan", 15);
+        Map<String,Integer> updateaccounts= Transference(accounts, "mati", "juan", 8);
+        assertEquals(15, updateaccounts.get("juan"));
+        assertEquals(0,  updateaccounts.get("mati"));
+    }
+    @Test
+    void testTransferenceexistence() {
+        Map<String , Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 15);
+        Map<String,Integer> updateaccounts = Transference(accounts, "Federico", "Gustavo", 20);
+        assertEquals(15, updateaccounts.get("juan"));
+        assertEquals(10, updateaccounts.get("mati"));
+    }
+
+    @Test
+    void testNegativeTransference() {
+        Map<String , Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 15);
+        Map<String,Integer> updateaccounts = Transference(accounts, "mati", "juan", -20);
+        assertEquals(15, updateaccounts.get("juan"));
+        assertEquals(10, updateaccounts.get("mati"));
+    }
+
+    @Test
+    void testSameperson() {
+        Map<String , Integer> accounts = new HashMap<>();
+        accounts.put("mati", 10);
+        accounts.put("juan", 15);
+        Map<String, Integer> updateaccounts= Transference(accounts, "mati", "mati", 20);
+        assertEquals(10, updateaccounts.get("mati"));
+        assertEquals(15, updateaccounts.get("juan"));
     }
 }
